39dcda4bcb5ac5fbc78d111fed0add40495e74fd
siyayang0420 <siyayang0420@live.com>
2018-10-09 18:47:22 -0700

    edit_2_index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//A
+function RandomRed(){
+  var red = Math.round(Math.random()*255)
+  return "rgb("+red+", 100,100)";
+}
+
 setInterval(()=>{
 	app.style.bakgroundColor = "rgb(255,222,255)";
 }, 500);
